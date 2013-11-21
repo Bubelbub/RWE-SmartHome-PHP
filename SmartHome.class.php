6ce178f92c72cc84eeed6be0681cfdcc27e01e22
Bubelbub <bubelbub@gmail.com>
2013-11-21 12:40:14 +0100

    Reformated code and added comments to all functions and variables

--- SmartHome.class.php
+++ SmartHome.class.php
@@ -1,41 +1,89 @@
 <?php
 
+/**
+ * Class SmartHome
+ *
+ * @author ansgr <http://www.rwe-smarthome-forum.de/user-ansgr>
+ * @author donenik <http://www.rwe-smarthome-forum.de/user-donenik>
+ * @author Bubelbub <bubelbub@gmail.com>
+ */
 class SmartHome
 {
+	/**
+	 * @var string the hostname or ip address of the central control unit
+	 */
 	private $host;
-	private $username;
-	private $password;
-	private $sessionId = false;
-	private $configurationVersion = false;
 
-	function __construct($host, $username, $password)
+	/**
+	 * @var string the username of a user which can use the central control unit (shc)
+	 */
+	private $username;
+
+	/**
+	 * @var string the password of the user with username $username
+	 */
+	private $password;
+
+	/**
+	 * @var boolean the session id of the user above
+	 */
+	private $sessionId = false;
+
+	/**
+	 * @var string the configuration version which readed from central control unit (shc)
+	 */
+	private $configurationVersion;
+
+	/**
+	 * @var \SimpleXMLElement the latest response
+	 */
+	private $response;
+
+	/**
+	 * @var string|integer|float the version of the central control unit (shc)
+	 */
+	private $version = '1.60';
+
+	/**
+	 * @param string $host the hostname or ip address of the central control unit
+	 * @param string $username the username of a user which can use the central control unit (shc)
+	 * @param string $password the password of the user with username $username
+	 */
+	public function __construct($host, $username, $password)
 	{
 		$this->host = $host;
 		$this->username = $username;
 		$this->password = $password;
 	}
 
-	function login()
+	/**
+	 * Log in the central control unit (shc)
+	 */
+	public function login()
 	{
 		$data = '<BaseRequest
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 		xmlns:xsd="http://www.w3.org/2001/XMLSchema"
 		xsi:type="LoginRequest"
-		RequestId="33300000-2200-1000-0000-'.substr(md5(uniqid()), 0, 12).'"
-		Version="1.60"
-		UserName="'.$this->username.'"
-		Password="'.base64_encode(hash('sha256', utf8_encode($this->password), true)).'"
+		RequestId="33300000-2200-1000-0000-' . substr(md5(uniqid()), 0, 12) . '"
+		Version="' . $this->version . '"
+		UserName="' . $this->username . '"
+		Password="' . base64_encode(hash('sha256', utf8_encode($this->password), true)) . '"
 		/>';
-		$response = $this->doRequest($data);
-		$a = $response->attributes();
+		$this->response = $this->doRequest($data);
+		$a = $this->getResponse()->attributes();
 		$this->sessionId = $a->SessionId;
 		$this->configurationVersion = $a->CurrentConfigurationVersion;
 	}
 
-	function doRequest($data)
+	/**
+	 * @param string $data the data which would be sended to central control unit (shc)
+	 * @return SimpleXMLElement the response of the central control unit (shc)
+	 */
+	public function doRequest($data)
 	{
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'https://'.$this->host.'/cmd');
+		curl_setopt($ch, CURLOPT_URL, 'https://' . $this->host . '/cmd');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
@@ -43,96 +91,124 @@ class SmartHome
 		curl_setopt($ch, CURLOPT_SSLVERSION, 3);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		$output = curl_exec($ch);
-		$info = curl_getinfo($ch);
 		curl_close($ch);
 		$xml = new SimpleXMLElement($output);
 		return $xml;
 	}
 
-	function getInformation()
+	/**
+	 * Get the information's of the central control unit
+	 *
+	 * @return SimpleXMLElement[] the information's of the central control unit
+	 */
+	public function getInformation()
 	{
 		$data = '<BaseRequest
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 		xmlns:xsd="http://www.w3.org/2001/XMLSchema"
 		xsi:type="ProbeShcRequest"
-		RequestId="33300000-2200-1000-0000-'.substr(md5(uniqid()), 0, 12).'"
-		Version="1.60"
-		SessionId="'.$this->sessionId.'"
+		RequestId="33300000-2200-1000-0000-' . substr(md5(uniqid()), 0, 12) . '"
+		Version="' . $this->version . '"
+		SessionId="' . $this->sessionId . '"
 		/>';
-		$response = $this->doRequest($data);
-		return $response->ShcInformation;
+		$this->response = $this->doRequest($data);
+
+		return $this->getResponse()->ShcInformation;
 	}
 
-	function getAllLogicalDeviceStates()
+	/**
+	 * Get all logical devices with their state(s)
+	 *
+	 * @return SimpleXMLElement[] all logical devices with their state(s)
+	 */
+	public function getAllLogicalDeviceStates()
 	{
 		$data = '<BaseRequest
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 		xmlns:xsd="http://www.w3.org/2001/XMLSchema"
 		xsi:type="GetAllLogicalDeviceStatesRequest"
-		RequestId="33300000-2200-1000-0000-'.substr(md5(uniqid()), 0, 12).'"
-		Version="1.60"
-		SessionId="'.$this->sessionId.'"
-		BasedOnConfigVersion="'.$this->configurationVersion.'"
+		RequestId="33300000-2200-1000-0000-' . substr(md5(uniqid()), 0, 12) . '"
+		Version="' . $this->version . '"
+		SessionId="' . $this->sessionId . '"
+		BasedOnConfigVersion="' . $this->configurationVersion . '"
 		/>';
-		$response = $this->doRequest($data);
-		return $response->States;
+		$this->response = $this->doRequest($data);
+
+		return $this->getResponse()->States;
 	}
 
-	function getConfiguration()
+	/**
+	 * Get the configuration of the central control unit (shc)
+	 *
+	 * @return SimpleXMLElement the configuration of the central control unit (shc)
+	 */
+	public function getConfiguration()
 	{
 		$data = '<BaseRequest
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 		xmlns:xsd="http://www.w3.org/2001/XMLSchema"
 		xsi:type="GetEntitiesRequest"
-		RequestId="33300000-2200-1000-0000-'.substr(md5(uniqid()), 0, 12).'"
-		Version="1.60"
-		SessionId="'.$this->sessionId.'"
-		BasedOnConfigVersion="'.$this->configurationVersion.'"
+		RequestId="33300000-2200-1000-0000-' . substr(md5(uniqid()), 0, 12) . '"
+		Version="' . $this->version . '"
+		SessionId="' . $this->sessionId . '"
+		BasedOnConfigVersion="' . $this->configurationVersion . '"
 		EntityType="Configuration"
 		/>';
-		$response = $this->doRequest($data);
+		$this->response = $this->doRequest($data);
 
-		return $response;
+		return $this->getResponse();
 	}
 
-	function switchActuator($logicalDeviceId, $on)
+	/**
+	 * Switches an actuator state
+	 *
+	 * @param string $logicalDeviceId the logical device id
+	 * @param boolean $on state to switch on
+	 */
+	public function switchActuator($logicalDeviceId, $on)
 	{
 		$data = '<BaseRequest
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"        
         xsi:type="SetActuatorStatesRequest"
-        Version="1.60"
-        RequestId="33300000-2200-1000-0000-'.substr(md5(uniqid()), 0, 12).'"
+        Version="' . $this->version . '"
+        RequestId="33300000-2200-1000-0000-' . substr(md5(uniqid()), 0, 12) . '"
         
-        SessionId="'.$this->sessionId.'"
-        BasedOnConfigVersion="'.$this->configurationVersion.'"
+        SessionId="' . $this->sessionId . '"
+        BasedOnConfigVersion="' . $this->configurationVersion . '"
         >
             <ActuatorStates>
             <LogicalDeviceState xsi:type="SwitchActuatorState"
-                LID="'.$logicalDeviceId.'"
-                IsOn="'.($on?'true':'false').'"    
+                LID="' . $logicalDeviceId . '"
+                IsOn="' . ($on ? 'true' : 'false') . '"
             />
             
             </ActuatorStates>
         </BaseRequest>
         ';
-		$response = $this->doRequest($data);
+		$this->response = $this->doRequest($data);
 	}
 
-	function setPointTemperature($logicalDeviceId, $pointTemperature)
+	/**
+	 * Set the temperature of an actuator
+	 *
+	 * @param string $logicalDeviceId the logical device id
+	 * @param float $pointTemperature the temperature to set
+	 */
+	public function setPointTemperature($logicalDeviceId, $pointTemperature)
 	{
 		$data = '<BaseRequest
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"        
         xsi:type="SetActuatorStatesRequest"
-        Version="1.60"
-        RequestId="33300000-2200-1000-0000-'.substr(md5(uniqid()), 0, 12).'"
+        Version="' . $this->version . '"
+        RequestId="33300000-2200-1000-0000-' . substr(md5(uniqid()), 0, 12) . '"
         
-        SessionId="'.$this->sessionId.'"
-        BasedOnConfigVersion="'.$this->configurationVersion.'"
+        SessionId="' . $this->sessionId . '"
+        BasedOnConfigVersion="' . $this->configurationVersion . '"
         >
             <ActuatorStates>
             <LogicalDeviceState xsi:type="RoomTemperatureActuatorState"
-                LID="'.$logicalDeviceId.'"
-                PtTmp="'.$pointTemperature.'"                
+                LID="' . $logicalDeviceId . '"
+                PtTmp="' . $pointTemperature . '"
                OpnMd="Auto"
                 WRAc="False"
             />
@@ -140,6 +216,16 @@ class SmartHome
             </ActuatorStates>
         </BaseRequest>
         ';
-		$response = $this->doRequest($data);
+		$this->response = $this->doRequest($data);
+	}
+
+	/**
+	 * Get the last response of an request
+	 *
+	 * @return \SimpleXMLElement the last response of shc
+	 */
+	public function getResponse()
+	{
+		return $this->response;
 	}
 }
